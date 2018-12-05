c610579e6d45338ab1ebc637eff2346aab8055be
falizyer <falizyer@outlook.com>
2018-12-05 18:24:25 +0300

    Order for heroes

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,20 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { Resolve } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Heroes } from "./index";
+import { Observable, of } from "rxjs";
+import { mergeMap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "application/json" })
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class HeroService implements Resolve<Heroes.HeroObject> {
+export class HeroService implements Resolve<Observable<any>> {
 
-  private heroesUrl = 'api/heroes';
+  private heroesUrl = "api/heroes";
 
   public constructor(private http: HttpClient) {
   }
 
-  public resolve(route, state): any {
+  public updateHero(hero: Heroes.HeroObject): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions);
+  }
+
+  public getHeroes(): Observable<any> {
     return this.http.get<Heroes.HeroObject[]>(this.heroesUrl);
+  }
+
+  /**
+   * @override
+   * @param route
+   * @param state
+   */
+  public resolve(route, state): Observable<any> {
+    return this.getHeroes()
+      .pipe(mergeMap((val: Array<any>) => of(val.sort((d1, d2) => d1.rank - d2.rank))));
   }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -6,18 +6,22 @@ import { Heroes } from "./heroes";
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
+
+  /**
+   * @override
+   */
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Mr. Nice', rank: 9 },
+      { id: 12, name: 'Narco', rank: 10 },
+      { id: 13, name: 'Bombasto', rank: 1 },
+      { id: 14, name: 'Celeritas', rank: 2 },
+      { id: 15, name: 'Magneta', rank: 7 },
+      { id: 16, name: 'RubberMan', rank: 1 },
+      { id: 17, name: 'Dynama', rank: 2 },
+      { id: 18, name: 'Dr IQ' , rank: 5},
+      { id: 19, name: 'Magma', rank: 8 },
+      { id: 20, name: 'Tornado', rank: 6 }
     ];
     return {heroes};
   }
